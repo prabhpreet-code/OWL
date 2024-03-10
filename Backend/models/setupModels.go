6b2faef2800dddf5f7c7d908a3440fef1948a310
Prabhpreet Singh <prabhpreetsingh.singh0203@gmail.com>
2024-03-10 19:23:53 +0530

    Fixed: fixed minor bugs

--- Backend/models/setupModels.go
+++ Backend/models/setupModels.go
@@ -1,6 +1,8 @@
 package models
 
 import (
+	"os"
+
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -16,7 +18,7 @@ func ConnectDatabase() {
 	// 	os.Getenv("DB_NAME"),
 	// 	os.Getenv("DB_PORT"),
 	// )
-	dsn := "postgresql://prabhpreetsingh.singh0203:qlgQcy6jw0ot@ep-mute-king-272149.ap-southeast-1.aws.neon.tech/neondb?"
+	dsn := os.Getenv("DB_LINK")
 	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
